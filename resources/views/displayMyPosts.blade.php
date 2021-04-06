979d22f0588284386ccd281256fc9564d381affc
Emily Quevedo <37810404+EmilyQue@users.noreply.github.com>
2021-04-05 21:56:06 -0700

    Update displayMyPosts.blade.php

--- resources/views/displayMyPosts.blade.php
+++ resources/views/displayMyPosts.blade.php
@@ -9,16 +9,16 @@
             <div class="row">
             @if(count($posts) != 0)
                 @foreach($posts as $c)
-                <div class="col-md-4">
+                    <div class="col-md-4">
                     <div class="card-content">
                         <div class="card-img">
-                            <img src="resources/images/{{$c['image']}}" alt="">
+                            <img src="resources/images/{{$c->getImage()}}" alt="">
                         </div>
                         <div class="card-desc">
-                            <h5 class="text-center">{{$c['title']}}</h5>
-                            <p class="text-center"><small><i>by</i> {{$c['firstName']}} {{$c['lastName']}} <i>on</i> {{$c['date']}}</small></p>
-                            <p>{{$c['description']}}</p>
-                            <form action='displaySinglePost'><input type='hidden' name='id' value={{$c['id']}}><input class="btn" type='submit' value='Go To Post'></form>
+                            <h5 class="text-center">{{$c->getTitle()}}</h5>
+                            <p class="text-center"><small><i>by</i> {{$c->getFirstName()}} {{$c->getLastName()}} <i>on</i> {{$c->getDate()}}</small></p>
+                            <p>{{$c->getDescription()}}</p>
+                            <form action='displaySinglePost'><input type='hidden' name='id' value={{$c->getId()}}><input class="btn" type='submit' value='Go To Post'></form>
                             {{-- <a href="displaySinglePost" class="btn-card">Go to Post</a> --}}
                         </div>
                     </div>
