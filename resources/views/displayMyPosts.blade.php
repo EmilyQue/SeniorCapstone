615d58d3b6482ca307d371f3e9724bee57e52440
Emily Quevedo <37810404+EmilyQue@users.noreply.github.com>
2021-04-05 21:53:22 -0700

    Update displayMyPosts.blade.php

--- resources/views/displayMyPosts.blade.php
+++ resources/views/displayMyPosts.blade.php
@@ -9,21 +9,20 @@
             <div class="row">
             @if(count($posts) != 0)
                 @foreach($posts as $c)
-                    <div class="col-md-4">
-                        <div class="card-content">
-                            <div class="card-img">
-                                <img src="resources/images/{{$c->getImage()}}" alt="">
-                            </div>
-                            <div class="card-desc">
-                                <h3>{{$c->getTitle()}}</h3>
-                                <p><small>By: {{$username}}</small></p>
-                                <p>{{$c->getDescription()}}</p>
-                                <p>Posted: {{$c->getDate()}}</p>
-                                <form action='displaySinglePost'><input type='hidden' name='id' value={{$c->getId()}}><input type='submit' value='Go To Post'></form>
-                                {{-- <a href="displaySinglePost" class="btn-card">Go to Post</a> --}}
-                            </div>
+                <div class="col-md-4">
+                    <div class="card-content">
+                        <div class="card-img">
+                            <img src="resources/images/{{$c['image']}}" alt="">
+                        </div>
+                        <div class="card-desc">
+                            <h5 class="text-center">{{$c['title']}}</h5>
+                            <p class="text-center"><small><i>by</i> {{$c['firstName']}} {{$c['lastName']}} <i>on</i> {{$c['date']}}</small></p>
+                            <p>{{$c['description']}}</p>
+                            <form action='displaySinglePost'><input type='hidden' name='id' value={{$c['id']}}><input class="btn" type='submit' value='Go To Post'></form>
+                            {{-- <a href="displaySinglePost" class="btn-card">Go to Post</a> --}}
                         </div>
                     </div>
+                </div>
                 @endforeach
             @else
                 <h3>No Blog Posts <br><small>Click the + to add your first post!</small></h3>
